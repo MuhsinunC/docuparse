4e2386fe9b70bbaf4d67b71056fa996fe4cf98dc
Muhsinun Chowdhury <11369216+MuhsinunC@users.noreply.github.com>
2025-04-05 16:54:33 -0700

    build images in parallel

--- run.sh
+++ run.sh
@@ -2,4 +2,10 @@
 # Simple script to build and run the Docker Compose services
 
 echo "Building and starting Docker containers..."
+
+# Use the bake feature to build the images in parallel (disable if you want to build sequentially)
+export COMPOSE_BAKE=true
+
 docker-compose up --build
+
+unset COMPOSE_BAKE
